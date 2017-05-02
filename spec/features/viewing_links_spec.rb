cbb41b4f1664c5860a376b25ff60a0225c061597
Cristina Iannone <iannonecristina@gmail.com>
2017-05-02 15:24:02 +0100

    Moves views into the app folder

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -4,7 +4,7 @@ feature 'Viewing links' do
 
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
 
-    visit '/links/index'
+    visit '/links'
     expect(page.status_code).to eq 200
 
     within 'ul#links' do
