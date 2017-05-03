1d31966c2090dec63c8868c7f37743161f9bdeaf
Sophie Mann <sklm@live.co.uk>
2017-05-03 18:08:36 +0100

    adds before do link creation to tag spec

--- spec/features/tags_spec.rb
+++ spec/features/tags_spec.rb
@@ -1,5 +1,9 @@
 feature 'Tags' do
 
+  before(:each) do
+    Link.create(url: "www.google.com", title: "Google", tags: [Tag.first_or_create(tag: "search")])
+  end
+
   scenario 'Can add tags to a link' do
     visit '/links/new'
     add_new_link
@@ -18,5 +22,6 @@ feature 'Tags' do
     click_button 'Save'
     visit '/tag/bubbles'
     expect(page).to have_content 'Blow Soap Bubbles'
+    expect(page).not_to have_content "Google"
   end
 end
